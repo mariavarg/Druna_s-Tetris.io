99288a4bd4647ef4ac03abc6cad209e37144ee09
mariavarg <75952007+mariavarg@users.noreply.github.com>
2023-10-22 20:32:03 +0300

    Update and rename index.py to index.html

--- index.py
+++
@@ -1,9 +0,0 @@
-import pygame, sys
-from game import Game
-from colors import Colors
-
-pygame.init()
-
-background_image = pygame.image.load("unsplash_upscayl.png")
-screen = pygame.display.set_mode((500, 620))
-pygame.display.set_caption("Druna_s Tetris")
