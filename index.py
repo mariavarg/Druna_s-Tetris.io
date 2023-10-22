d642a3bc37f5e1c42fa84dd6f181331b5e7c1cc7
mariavarg <75952007+mariavarg@users.noreply.github.com>
2023-10-22 20:25:16 +0300

    Update index.py

--- index.py
+++ index.py
@@ -5,3 +5,5 @@ from colors import Colors
 pygame.init()
 
 background_image = pygame.image.load("unsplash_upscayl.png")
+screen = pygame.display.set_mode((500, 620))
+pygame.display.set_caption("Druna_s Tetris")
